dc9140fa95456abd864e1809c9535f127a6500f3
ShivaKumarAnandam <shivakumaraanandham8@gmail.com>
2023-09-28 19:10:45 +0530

    "currency-exchange microservice"

---
+++ src/main/java/com/currency/exchange/CurrencyExchange.java
@@ -0,0 +1,27 @@
+package com.currency.exchange;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class CurrencyExchange {
+	@Id
+	private Long id;
+	@Column(name = "currency_from")
+	private String from;
+	@Column(name = "currency_to")
+	private String to;
+	@Column(name = "conversion_multiple")
+	private BigDecimal conversionMultiple;
+
+}
